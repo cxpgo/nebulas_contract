ade42ff4434d6c96fb14617ad5920d848bdc1c85
iiswho666 <5031037@qq.com>
2018-05-14 20:26:17 +0800

    1

--- FootballContract.js
+++ FootballContract.js
@@ -3,7 +3,7 @@
 var TeamNumber = {1:"英",2:"德",3:"法",4:"荷",5:"西",6:"巴",7:"阿",8:"意",9:"葡",10:"美"};
 var GameSchedule = {1:[1,2],2:[3,4],3:[5,6],4:[7,8],5:[9,10]};
 
-var GameContent = function (obj) {
+var BetContent = function (obj) {
     if(!obj){
         throw new Error("GameContent not have init params!");
     }
@@ -22,9 +22,10 @@ var GameContent = function (obj) {
     this.BBet = new BigNumber(0);
 };
 
-var PlayerBetContent = function () {
+var PlayerBetInfo = function () {
 
     this.from = "";
+    this.betContentIndex = 0;//下注编号
     this.ABet = new BigNumber(0);
     this.BBet = new BigNumber(0);
     this.ADirection = 0;
@@ -33,20 +34,31 @@ var PlayerBetContent = function () {
 };
 
 var FootballContract = function () {
-  // LocalContractStorage.defineMapProperty(this, "bankVault", {
-  //   parse: function (text) {
-  //     return new DepositeContent(text);
-  //   },
-  //   stringify: function (o) {
-  //     return o.toString();
-  //   }
-  // });
+  LocalContractStorage.defineMapProperty(this, "gameScheduleMap");
 };
 
 FootballContract.prototype = {
     init: function () {
       //TODO:
+      this.adminAddress = "n1HH4bfu2w1ZC22MfSKH7YjNqtyoNHHHrY3";
     },
+
+    addBetSession:function(betIndex){
+      var index = parseInt(betIndex);
+      var gameSchedule = GameSchedule[index];
+      if(!gameSchedule){
+        throw new Error("no have No." + betIndex + " by GameSchedule!");
+      }
+
+      var gameInfo = {"index":index};
+
+      var betContent = new BetContent(gameInfo);
+      betContent.AName = TeamNumber[gameSchedule[0]];
+      betContent.BName = TeamNumber[gameSchedule[1]];
+      this.gameScheduleMap.put(index,betContent);
+
+      return "addBetSession success :" + JSON.stringify(betContent);
+    }
 }
 
 
